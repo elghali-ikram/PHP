4563905bd37e7725f4bdaaa6e40881d063ce8884
elghali-ikram <ikram.elghali.solicode@gmail.com>
2023-01-17 15:24:08 +0100

    php

--- PHPBRIEF.php
+++ PHPBRIEF.php
@@ -24,10 +24,10 @@
               <div class=" d-flex gap-4">
                 <img src="images.jfif" class="rounded-circle" width="40px" height="40px">
                 <label class="align-self-center">Morrocco</label>
-                <input type="number" class="form-control"  name="morroco1">
+                <input type="number" class="form-control" value="<?php if(isset($_POST['morroco1'])) echo $_POST['morroco1']; ?>" name="morroco1">
               </div>
               <div class=" d-flex gap-4">
-                <input type="number" class="form-control"   name="croatia1">
+                <input type="number" class="form-control" value="<?php if(isset($_POST['croatia1'])) echo $_POST['croatia1']; ?>" name="croatia1">
                 <label class="align-self-center">Croatia</label>
                 <img src="croatia.png" class="rounded-circle" width="40px" height="40px">
               </div>
@@ -41,10 +41,10 @@
               <div class="d-flex gap-4">
                 <img src="belgique.png" class="rounded-circle" width="40px" height="40px">
                 <label class="align-self-center">Belgique</label>
-                <input type="number" class="form-control"  name="belgique1">
+                <input type="number" class="form-control" value="<?php if(isset($_POST['belgique1'])) echo $_POST['belgique1']; ?>" name="belgique1">
               </div>
               <div class=" d-flex gap-4">
-                <input type="number" class="form-control "  name="canada1">
+                <input type="number" class="form-control" value="<?php if(isset($_POST['canada1'])) echo $_POST['canada1']; ?>" name="canada1">
                 <label class="align-self-center">Canada</label>
                 <img src="canada.jfif" class="rounded-circle" width="40px" height="40px">
 
@@ -59,10 +59,10 @@
               <div class=" d-flex gap-4">
                 <img src="belgique.png" class="rounded-circle" width="40px" height="40px">
                 <label class="align-self-center">Belgique</label>
-                <input type="number" class="form-control"  name="beljique2">
+                <input type="number" class="form-control" value="<?php if(isset($_POST['belgique2'])) echo $_POST['belgique2']; ?>" name="belgique2">
               </div>
               <div class=" d-flex gap-4">
-                <input type="number" class="form-control"  name="morroco2">
+                <input type="number" class="form-control" value="<?php if(isset($_POST['morroco2'])) echo $_POST['morroco2']; ?>" name="morroco2">
                 <label class="align-self-center">Morrocco</label>
                 <img src="images.jfif" class="rounded-circle" width="40px" height="40px">
 
@@ -79,10 +79,10 @@
               <div class=" d-flex gap-4">
                 <img src="croatia.png" class="rounded-circle" width="40px" height="40px">
                 <label class="align-self-center">Croatia</label>
-                <input type="number" class="form-control"  name="croatia2">
+                <input type="number" class="form-control" value="<?php if(isset($_POST['croatia2'])) echo $_POST['croatia2']; ?>" name="croatia2">
               </div>
               <div class=" d-flex gap-4">
-                <input type="number" class="form-control"  name="canada2">
+                <input type="number" class="form-control" value="<?php if(isset($_POST['canada2'])) echo $_POST['canada2']; ?>" name="canada2">
                 <label class="align-self-center">Canada</label>
                 <img src="canada.jfif" class="rounded-circle" width="40px" height="40px">
               </div>
@@ -96,10 +96,10 @@
               <div class=" d-flex gap-4">
                 <img src="croatia.png" class="rounded-circle" width="40px" height="40px">
                 <label class="align-self-center">Croatia</label>
-                <input type="number" class="form-control"  name="croatia3">
+                <input type="number" class="form-control" value="<?php if(isset($_POST['croatia3'])) echo $_POST['croatia3']; ?>" name="croatia3">
               </div>
               <div class=" d-flex gap-4">
-                <input type="number" class="form-control" name="belgique3">
+                <input type="number" class="form-control" value="<?php if(isset($_POST['belgique3'])) echo $_POST['belgique3']; ?>" name="belgique3">
                 <label class="align-self-center">Belgique</label>
                 <img src="belgique.png" class="rounded-circle" width="40px" height="40px">
               </div>
@@ -114,10 +114,10 @@
               <div class=" d-flex gap-4">
                 <img src="canada.jfif" class="rounded-circle" width="40px" height="40px">
                 <label class="align-self-center">Canada</label>
-                <input type="number" class="form-control"  name="canada3">
+                <input type="number" class="form-control" value="<?php if(isset($_POST['canada3'])) echo $_POST['canada3']; ?>" name="canada3">
               </div>
               <div class=" d-flex gap-4">
-                <input type="number" class="form-control"  name="morroco3">
+                <input type="number" class="form-control" value="<?php if(isset($_POST['morroco3'])) echo $_POST['morroco3']; ?>" name="morroco3">
                 <label class="align-self-center">Morrocco</label>
                 <img src="images.jfif" class="rounded-circle" width="40px" height="40px">
               </div>
@@ -160,45 +160,88 @@
       </table>
     </div>
   </div>
-  <?php 
-    $body = print_r($_POST, true); 
+  <?php
+
+  function getdata()
+  {
     $count = 0;
     $array1 = array();
     foreach ($_POST as $key => $value) {
-      global $count,$array1;
-      if(!empty($value))
-      {
-        $array1["$key"]="$value";
+      global $count, $array1;
+      if (!empty($value)) {
+        $array1["$key"] = "$value";
         // array_push($array1["$key"], );
-         $count++;    
+        $count++;
       }
     }
-  $valuesd = array_values($array1);
-  $keysd = array_keys($array1);
-    // foreach ($array1 as $key => $value) {
-    //   echo $value;
-    //   # code...
-    // }
-    for ($i=0; $i < count($valuesd)-1 ; $i+=2) {
+    $valuesd = array_values($array1);
+    $keysd = array_keys($array1);
+    $standings = array();
+    $table1 = array();
+
+    foreach ($keysd as $team) {
+      $standings[$team] = array("points" => 0, "goals" => 0);
+    }
+    for ($i = 0; $i < count($valuesd) - 1; $i += 2) {
       echo $valuesd[$i];
-      echo $valuesd[$i+1];
-      if ($valuesd[$i] > $valuesd[$i+1]) {
-         echo "sah";
+      echo $valuesd[$i + 1];
+      if ($valuesd[$i] > $valuesd[$i + 1]) {
+        $standings[$keysd[$i]]["points"] += 3;
+        $standings[$keysd[$i]]["goals"] += $valuesd[$i];        
+        $standings[$keysd[$i+1]]["goals"] += $valuesd[$i+1];
+        echo "sah";
         # code...
       }
-    // foreach ($array1[$i] as $key => $value) {
-    //   echo $key;
+      elseif($valuesd[$i] < $valuesd[$i+1])
+      {
+        $standings[$keysd[$i+1]]["points"] += 3;
+        $standings[$keysd[$i+1]]["goals"] += $valuesd[$i+1];
+        $standings[$keysd[$i]]["goals"] += $valuesd[$i];        
+       echo "dkhel";
+      }
+      elseif($valuesd[$i] == $valuesd[$i+1])
+      {
+        $standings[$keysd[$i+1]]["points"] +=1;
+        $standings[$keysd[$i]]["points"] +=1;
+        $standings[$keysd[$i+1]]["goals"] += $valuesd[$i];
+        $standings[$keysd[$i]]["goals"] += $valuesd[$i+1];
+      }
+    }
+
+    print_r($standings) ;
+    // $keyteam = array_keys($standings);
+    // for ($i=0; $i < count($keyteam)-1 ; $i++) {
+    //   for ($j=$i+1; $j <count($keyteam); $j++) { 
+    //     $key1 = substr($keyteam[$i], 0 , -1);
+    //     $key2 = substr($keyteam[$j], 0 , -1);
+    //     echo $key1 . "  " . $key2."</br>";
+    //     if($key1==$key2)
+    //     {
+
+    //       echo $standings[$keyteam[$i]]["goals"];
+    //       echo $standings[$keyteam[$j]]["goals"];
+    //       array_push($table1, $key1);
+    //       echo "kayn </br>";
+    //     }
+    //     else
+    //     {
+    //       echo "mkaynch </br>";
+    //     }
+    //   }
+
     //   # code...
     // }
+    // print_r($table1)."</br>";
   }
- 
-/*       foreach ($array1[$i] as $key => $value) {
-        foreach ($array1[$i+1] as $x => $y) {
-          echo $key."</br>";
-          echo $x."</br>";
-          # code...
-        }
-      } */
+  getdata();
+
+  // foreach ($array1 as $key => $value) {
+  //   echo $value;
+  //   # code...
+  // }
+
+
+
 
 
 
